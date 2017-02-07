cda19e46d6ff85e923c47064b73b723d00a5717f
cight <necronezum@gmail.com>
2017-02-07 16:50:32 -0500

    Jade to EJS

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page */
-router.get('/', function(req, res) {
+//GET home page
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* GET about page */
-router.get('/about', function(req, res) {
+//GET about page
+router.get('/about', function(req, res, next) {
     res.render('about', { title: 'About' });
 });
 
-/* GET projects page */
-router.get('/projects', function(req, res) {
+//GET projects page
+router.get('/projects', function(req, res, next) {
     res.render('projects', { title: 'Projects' });
 });
 
-/* GET services page */
-router.get('/services', function(req, res) {
+//GET services page
+router.get('/services', function(req, res, next) {
     res.render('services', { title: 'Services' });
 });
 
-/* GET skills page */
-router.get('/skills', function(req, res) {
+//GET skills page
+router.get('/skills', function(req, res, next) {
     res.render('skills', { title: 'Skills' });
 });
 
-/* GET contact page */
-router.get('/contact', function(req, res) {
+//GET contact page
+router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Contact' });
 });
 
