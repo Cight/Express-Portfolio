45268614e4e1558e3a0f7cc99482adeeb7d16f28
cight <necronezum@gmail.com>
2017-02-07 21:09:13 -0500

    Routing Complete

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 //GET home page
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Home' });
 });
 
 //GET about page
-router.get('/about', function(req, res, next) {
+router.get('/about', function(req, res) {
     res.render('about', { title: 'About' });
 });
 
 //GET projects page
-router.get('/projects', function(req, res, next) {
+router.get('/projects', function(req, res) {
     res.render('projects', { title: 'Projects' });
 });
 
 //GET services page
-router.get('/services', function(req, res, next) {
+router.get('/services', function(req, res) {
     res.render('services', { title: 'Services' });
 });
 
 //GET skills page
-router.get('/skills', function(req, res, next) {
+router.get('/skills', function(req, res) {
     res.render('skills', { title: 'Skills' });
 });
 
 //GET contact page
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function(req, res) {
     res.render('contact', { title: 'Contact' });
 });
 
